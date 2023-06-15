1cbc670d5e8ad4cc3b487e3105fcdbfa02f11ef5
slkpddm <slkpddm@gmail.com>
2023-06-15 10:42:02 +0530

    Added components , enitity and pipelines files

---
+++ sensor/entity/artifact_entity.py
@@ -0,0 +1,15 @@
+
+class DataIngestionArtifact:
+    pass
+class DataValidationArtifact:
+    pass
+class DataTransformationArtifact:
+    pass
+class ModelTrainerArtifact:
+    pass
+class ModelEvaluationArtifact:
+    pass
+class ModelPusherArtifact:
+    pass
+
+# orlese we can use class DataIngestionArtifact:... instead of writing pass statement again
